fcdf70a943346696f4d22bdbe7ff489147501b11
infinites5 <89767001+infinites5@users.noreply.github.com>
2021-11-16 16:59:42 +0530

    Update About.js

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -15,24 +15,24 @@ const About = () => {
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/dviij6cqr/image/upload/v1633644045/IMG-20190410-WA0012_rqzqa6.jpg"
+              src=""
               alt="Founder"
             />
             <span>
-              This Rental E-commerce is bulit by <a href="https://vijay-anand.netlify.app/">Vijay Anand.M.</a><br/>
+              This Rental E-commerce is bulit by <a href="">Nivetha.</a><br/>
                Certified Mern stack developer
             </span>
           </div>
           <div className="aboutSectionContainer2">
             <Typography component="h2">Contact me @..</Typography>
             <a
-              href="https://www.linkedin.com/in/vijay-anand-305b4b17b/"
+              href="
               target="blank"
             >
               <Linkedin className="youtubeSvgIcon" />
             </a>
 
-            <a href="https://github.com/VijayAnand2408/" target="blank">
+            <a href="" target="blank">
               <Github className="instagramSvgIcon" />
             </a>
           </div>
